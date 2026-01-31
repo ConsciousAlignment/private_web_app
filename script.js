8602e8ff0b5c7ebcd92d0da465c76690f3ee4ff4
ConsciousAlignment <andrianapapa136@gmail.com>
2026-01-31 06:28:45 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,17 +1,8 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyDMsNzep2kpFANzXZfH_J56i2Bu0FqpB2A",
-  authDomain: "private-web-app-9903c.firebaseapp.com",
-  projectId: "private-web-app-9903c",
-  storageBucket: "private-web-app-9903c.firebasestorage.app",
-  messagingSenderId: "641087792177",
-  appId: "1:641087792177:web:36ff5fcf0707c664fae110"
+  apiKey: "YOUR_REAL_API_KEY",
+  authDomain: "YOUR_PROJECT.firebaseapp.com",
+  projectId: "YOUR_PROJECT",
+  storageBucket: "YOUR_PROJECT.appspot.com",
+  messagingSenderId: "XXXXXX",
+  appId: "1:XXXX:web:XXXX"
 };
-
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
